4e589310fd41955bb0daf78de4f371e259197875
zhushaohz <zhushaohz@163.com>
2020-04-02 12:18:14 +0800

    初步搭建

---
+++ go.js
@@ -0,0 +1,4 @@
+setTimeout("go()","5000"); 
+function go(){
+    window.location.href='./mall';
+}
---
+++ mall/index.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+?>
+
+<html>
+
+<head>
+    <title>ZZMall</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+
+    <link rel="shortcut icon" href="rain.ico" />
+
+    <link href="mall.css?v=1963" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.staticfile.org/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdn.staticfile.org/popper.js/1.15.0/umd/popper.min.js"></script>
+    <script src="https://cdn.staticfile.org/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+    <div class="container">
+        <ul class="nav nav-tabs justify-content-end">
+            <li class="nav-item">
+                <a class="nav-link" href="#">购物车</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="../signup/">注册</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="../login/">登录</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">联系我们</a>
+            </li>
+        </ul>
+    </div>
+    <script type="text/javascript" src="mall.js"></script>
+</body>
+
+</html>
