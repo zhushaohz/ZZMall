695d5ff5c7c782c852c29d94a16f37553403404f
zhushaohz <zhushaohz@163.com>
2020-04-04 10:37:13 +0800

    首页特效

--- go.js
+++ go.js
@@ -1,4 +1,20 @@
-setTimeout("go()","5000"); 
+setTimeout("go()","5000000"); 
 function go(){
     window.location.href='./mall';
+    
 }
+function showNotification() {
+        window.Notification.permission = "granted";
+        if(window.Notification) {
+            if(window.Notification.permission == "granted") {
+                var notification = new Notification('你有一条新信息', {
+                    body: "你好我是王小婷",
+
+                    icon: "img/1.jpg"
+                });
+                setTimeout(function() { notification.close(); }, 5000);
+            } else {
+                window.Notification.requestPermission();
+            }
+        } else alert('你的浏览器不支持此消息提示功能，请使用chrome内核的浏览器！');
+    };
